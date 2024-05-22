3c40c12c6bead63a52022addcd735e0e458aac20
rahulurkude782 <rahulurkude782@gmail.com>
2024-05-22 16:53:34 +0530

    Implement Sign-In AuthForm Component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,143 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address." }),
+  password: z.string().min(8, {
+    message: "Password must be 8 character long.",
+  }),
+});
+
+const AuthForm = ({ type }: { type: string }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 max-sm-gap-5">
+        <Link href="/" className="flex items-center cursor-pointer gap-1">
+          <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon" />
+          <h1 className="text-24 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+          </h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 md-text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "SignIn" : "SignUp"}
+          </h1>
+          <p className="text-16 font-normal text-gray-900">
+            {user
+              ? "Link your account to get started."
+              : "Please enter your details."}
+          </p>
+        </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4">{/* PLAID LINK */}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="form-item">
+                    <FormLabel className="form-label">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="input-class"
+                        placeholder="Enter your email..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="form-message" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="form-item">
+                    <FormLabel className="form-label">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="input-class"
+                        placeholder="Enter your password..."
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="form-message" />
+                  </FormItem>
+                )}
+              />
+              <div className="flex flex-col gap-4">
+                <Button type="submit" className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" />
+                      &nbsp;Loading...
+                    </>
+                  ) : (
+                    "Submit"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </>
+      )}
+      <footer className="flex justify-center gap-1">
+        <p className="text-14 font-normal text-gray-600">
+          {type === "sign-in"
+            ? "Don't have an account ?"
+            : "Already have an account"}
+        </p>
+        <Link
+          className="form-link"
+          href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+        >
+          {type === "sign-in" ? "Sign Up" : "Sign In"}
+        </Link>
+      </footer>
+    </section>
+  );
+};
+
+export default AuthForm;
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,5 +1,6 @@
 import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const BankCard = ({
@@ -8,49 +9,51 @@ const BankCard = ({
   showBalance = true,
 }: CreditCardProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="bank-card">
-        <div className="bank-card_content">
-          <div>
-            <h1 className="text-16 font-semibold text-white">{userName}</h1>
-            <p className="font-ibm-plex-serif font-semibold font-16 text-white">
-              {formatAmount(account.currentBalance)}
-            </p>
-          </div>
-          <article className="flex flex-col gap-2">
-            <div className="flex justify-between">
-              <h1 className="text-12 font-semibold text-white">{userName}</h1>
-              <h1 className="text-12 font-semibold text-white">●● / ●●</h1>
+    <Link href={"/my-banks"}>
+      <div className="flex flex-col">
+        <div className="bank-card">
+          <div className="bank-card_content">
+            <div>
+              <h1 className="text-16 font-semibold text-white">{userName}</h1>
+              <p className="font-ibm-plex-serif font-semibold font-16 text-white">
+                {formatAmount(account.currentBalance)}
+              </p>
             </div>
-            <p className="text-12 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-14">1234</span>
-            </p>
-          </article>
-        </div>
-        <div className="bank-card_icon">
+            <article className="flex flex-col gap-2">
+              <div className="flex justify-between">
+                <h1 className="text-12 font-semibold text-white">{userName}</h1>
+                <h1 className="text-12 font-semibold text-white">●● / ●●</h1>
+              </div>
+              <p className="text-12 font-semibold tracking-[1.1px] text-white">
+                ●●●● ●●●● ●●●● <span className="text-14">1234</span>
+              </p>
+            </article>
+          </div>
+          <div className="bank-card_icon">
+            <Image
+              src={"/icons/Paypass.svg"}
+              width={20}
+              height={24}
+              alt="paypass"
+            />
+            <Image
+              src={"/icons/mastercard.svg"}
+              width={45}
+              height={32}
+              alt="msaatercard"
+              className="ml-5"
+            />
+          </div>
           <Image
-            src={"/icons/Paypass.svg"}
-            width={20}
-            height={24}
-            alt="paypass"
-          />
-          <Image
-            src={"/icons/mastercard.svg"}
-            width={45}
-            height={32}
-            alt="msaatercard"
-            className="ml-5"
+            src={"/icons/lines.png"}
+            width={316}
+            height={190}
+            alt="lines"
+            className="absolute top-0 left-0"
           />
         </div>
-        <Image
-          src={"/icons/lines.png"}
-          width={316}
-          height={190}
-          alt="lines"
-          className="absolute top-0 left-0"
-        />
       </div>
-    </div>
+    </Link>
   );
 };
 
